a41892363bc7fc2294faf68167828a9d0994be77
MayDead2Much <maydead2much@gmail.com>
2018-10-02 22:07:29 +0200

    Advanced Linux Installation

--- install.sh
+++ install.sh
@@ -5,3 +5,23 @@ printf '\033[0;34m Installing GLAD\n\033[0;31m'
 mv ./glad /usr/include
 
 printf '\033[0;34m Finished Installing GLAD\n'
+
+mkdir $HOME'/NEON Engine'
+mv ./assets $HOME'/NEON Engine/'
+mv 'NEON Engine' $HOME'/NEON Engine/'
+
+printf '\033[0;34m Engine Installation successfully finished!'
+
+echo > $HOME'/Desktop/NEON Engine.desktop'
+
+echo "[Desktop Entry]
+Name=NEON Engine
+Comment=Perfect game engine. Suits all your needs!
+GenericName=NEON
+Exec=/home/martin/NEONEngine/NEONEngine
+Terminal=true
+Path=/home/martin/NEONEngine
+Icon=unity-editor-icon
+Type=Application
+Categories=Development;Utility;
+Name[en_US]=NEON" > $HOME'/Desktop/NEONEngine.desktop'
