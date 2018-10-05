46642bcd9a0b28a8fc5cb279237519c55c9dde85
MayDead2Much <maydead2much@gmail.com>
2018-10-05 18:13:14 +0200

    GLAD installation fix

--- install.sh
+++ install.sh
@@ -2,30 +2,31 @@
 printf '\033[0;34m Installation Started!\n'
 printf '\033[0;34m Installing GLAD\n\033[0;31m'
 
-mv ./glad /usr/include
+mv ./GLAD/glad /usr/include
+mv ./GLAD/KHR /usr/include
 
 printf '\033[0;34m Finished Installing GLAD\n'
 
-make build
+#make build
 
-mkdir $HOME'/NEONEngine'
-mv ./assets $HOME'/NEONEngine/'
-mv 'NEONEngine' $HOME'/NEONEngine/'
+#mkdir $HOME'/NEONEngine'
+#mv ./assets $HOME'/NEONEngine/'
+#mv 'NEONEngine' $HOME'/NEONEngine/'
 
-mkdir $HOME'/NEONEngine/projects/'
+#mkdir $HOME'/NEONEngine/projects/'
 
-printf '\033[0;34m Engine Installation successfully finished!'
+#printf '\033[0;34m Engine Installation successfully finished!'
 
-echo > $HOME'/Desktop/NEON Engine.desktop'
+#echo > $HOME'/Desktop/NEON Engine.desktop'
 
-echo "[Desktop Entry]
-Name=NEON Engine
-Comment=Perfect game engine. Suits all your needs!
-GenericName=NEON
-Exec=/home/martin/NEONEngine/NEONEngine
-Terminal=true
-Path=/home/martin/NEONEngine
-Icon=unity-editor-icon
-Type=Application
-Categories=Development;Utility;
-Name[en_US]=NEON" > $HOME'/Desktop/NEONEngine.desktop'
+#echo "[Desktop Entry]
+#Name=NEON Engine
+#Comment=Perfect game engine. Suits all your needs!
+#GenericName=NEON
+#Exec=/home/martin/NEONEngine/NEONEngine
+#Terminal=true
+#Path=/home/martin/NEONEngine
+#Icon=unity-editor-icon
+#Type=Application
+#Categories=Development;Utility;
+#Name[en_US]=NEON" > $HOME'/Desktop/NEONEngine.desktop'
